82ce4951bfac2a83ee833e69b846a171a0477811
arphercn <201313488@qq.com>
2017-12-27 15:19:15 +0800

    second version

--- server.js
+++ server.js
@@ -4,35 +4,84 @@ var http = require('http'),
 const PORT = 9991,
       PATH = '/var/www/html/scheduled'
 
-var deployServer = http.createServer(function(request, response) {
-  if (request.url.search(/deploy\/?$/i) > 0) {
+var server = http.createServer().listen(PORT)
 
-    var commands = [
-      'cd ' + PATH,
-      'git pull'
-    ].join(' && ')
 
-    exec(commands, function(err, out, code) {
-      if (err instanceof Error) {
-        response.writeHead(500)
-        response.end('Server Internal Error')
-        throw err
-      }
-      response.writeHead(200)
-      response.end('Deploy Done')
-    })
+server.on('request', function(req, res) {
 
-  } else {
+    var msg = ''
+        
+    function _dump(msg) {
+        var time = new Date().toLocaleString()
+        console.log('['+time+'] ' + msg)
+        res.end('['+time+'] ' + msg)
+    }
 
-	if (request.url.search(/status\/?$/i) > 0) {
-		response.writeHead(200)
-		response.end('Normal Status')
-	}
+    if (req.url.search(/deploy\/?$/i) > 0) {
 
-    response.writeHead(404)
-    response.end('Not Found')
-  }
+        if (req.method == 'POST') {
+            var body = ''
+
+            req.on('data', function(data) {
+                body += data
+            })
+
+            req.on('end', function () {
+                var obj = JSON.parse(body),
+                    version = '',
+                    commands = ''
+                if (obj.push.changes[0].old) {
+                    version = obj.push.changes[0].old.name
+
+                    if (version == 'master') {
+                        commands = [
+                        'cd ' + PATH,
+                        'git checkout master',
+                        'git pull'
+                        ].join(' && ')
+
+                        msg = 'pull ' + version
+
+                        _dump(msg)
+                        
+                    } else if (version == 'develop') {
+                        commands = [
+                        'cd ' + PATH,
+                        'git checkout develop',
+                        'git pull'
+                        ].join(' && ')
+
+                        msg = 'pull ' + version
+                        _dump(msg)
+
+                    } else {
+                        msg = 'not found version'
+                        _dump(msg)
+                    }
+
+                    exec(commands, function(err, out, code) {
+                        if (err instanceof Error) {
+                            msg = 'Execute Commands Error'
+                            _dump(msg)
+                            //throw err
+                        }
+                    })
+
+                }
+
+            }) // end request on end
+
+        } // end POST
+
+
+
+    } else if (req.url.search(/status\/?$/i) > 0) {
+        res.writeHead(200)
+        msg = 'normal status'
+        _dump(msg)
+    } else {
+        msg = 'other request'
+        _dump(msg)
+    }
 
 })
-
-deployServer.listen(PORT)
